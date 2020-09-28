2faff53b701477008a09a25de3f4a605d937e031
Andrey <a.ryzhenkov@qiwi.com>
2020-09-28 18:02:49 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <pre>{{ myArray }}</pre>
+    <SomeComponent :parent-array="[...myArray]" />
+  </div>
+</template>
+
+<script>
+import SomeComponent from "./components/SomeComponent.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      myArray: [
+        {
+          name: "Name1",
+          value: "Value1"
+        },
+        {
+          name: "Name2",
+          value: "Value2"
+        }
+      ]
+    };
+  },
+  components: {
+    SomeComponent
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SomeComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <template v-for="(item, index) in localArray">
+      <input type="text" v-model="item.value" :key="`some_${index}`" />
+    </template>
+    <pre>
+      {{ this.localArray }}
+    </pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      localArray: this.parentArray
+    };
+  },
+  props: {
+    parentArray: {
+      type: Array
+    }
+  }
+};
+</script>
+
+<style scoped></style>
